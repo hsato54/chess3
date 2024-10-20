8d2264034a9adb1b8234e5e8db0b4be05a8d37f7
hsato54 <hsato54@byu.edu>
2024-10-19 19:58:22 -0600

    beginning of phase 3

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,15 @@
 import chess.*;
+import server.Server;
 
+//public class Main {
+//    public static void main(String[] args) {
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
+//    }
+//}
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        Server server = new Server(); // create the Server object
+        server.run(8080); // call the run method with the port (8080)
     }
 }
