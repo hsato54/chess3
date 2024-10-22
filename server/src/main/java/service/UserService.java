65483852d7e1f066499fd323a654ecb0ccd02dd2
hsato54 <hsato54@byu.edu>
2024-10-21 20:27:23 -0600

    userservice done i think

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -27,7 +27,7 @@ public class UserService {
         userDAO.createUser(user);
 
         String authToken = UUID.randomUUID().toString();
-        AuthData authData = new AuthData(user.username(), authToken);
+        AuthData authData = new AuthData(authToken, user.username());
         authDAO.createAuth(authData);
         return authData;
     }
@@ -35,6 +35,23 @@ public class UserService {
     public AuthData login(UserData user) throws DataAccessException{
         UserData existingUser = userDAO.getUser(user.username());
 
+        if (existingUser == null || !existingUser.password().equals(user.password())) {
+            throw new DataAccessException("Invalid username or password.");
+        }
+
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, user.username());
+        authDAO.createAuth(authData);
+        return authData;
+    }
+
+    public void logout(AuthData auth){
+        authDAO.deleteAuth(auth.authToken());
+    }
+
+    public void clear(){
+        userDAO.clear();
+        authDAO.clear();
     }
 
 
