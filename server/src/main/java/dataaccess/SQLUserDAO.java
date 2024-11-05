256e6b19547a7cd87b9aef4c06dbd64339c37851
hsato54 <hsato54@byu.edu>
2024-11-04 19:46:10 -0700

    sql user i had already done

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,93 +1,80 @@
-//package dataaccess;
-//
-//import model.UserData;
-//
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.SQLException;
-//
-//public class SQLUserDAO implements UserDAO {
-//
-//    public SQLUserDAO() {
-//        try {
-//            Database.createDatabase();
-//        } catch (DataAccessException ex) {
-//            throw new RuntimeException(ex);
-//        }
-//
-//        try (Connection conn = Database.getConnection()) {
-//            String createUsersTableSQL = """
-//                    CREATE TABLE IF NOT EXISTS users (
-//                        username VARCHAR(255) NOT NULL,
-//                        password VARCHAR(255) NOT NULL,
-//                        email VARCHAR(255),
-//                        PRIMARY KEY (username)
-//                    )""";
-//
-//            try (PreparedStatement createTableStatement = conn.prepareStatement(createUsersTableSQL)) {
-//                createTableStatement.executeUpdate();
-//            }
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    @Override
-//    public void createUser(UserData user) throws DataAccessException {
-//        String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
-//
-//        try (Connection conn = Database.getConnection();
-//             PreparedStatement stmt = conn.prepareStatement(sql)) {
-//
-//            stmt.setString(1, user.username());
-//            stmt.setString(2, user.password());
-//            stmt.setString(3, user.email());
-//
-//            stmt.executeUpdate();
-//        } catch (SQLException e) {
-//            throw new DataAccessException("Error inserting user into database.");
-//        }
-//    }
-//
-//    @Override
-//    public UserData getUser(String username) throws DataAccessException {
-//        String sql = "SELECT username, password, email FROM users WHERE username = ?";
-//
-//        try (Connection conn = Database.getConnection();
-//             PreparedStatement stmt = conn.prepareStatement(sql)) {
-//
-//            stmt.setString(1, username);
-//            var rs = stmt.executeQuery();
-//
-//            if (rs.next()) {
-//                return new UserData(
-//                        rs.getString("username"),
-//                        rs.getString("password"),
-//                        rs.getString("email")
-//                );
-//            } else {
-//                throw new DataAccessException("User not found.");
-//            }
-//        } catch (SQLException e) {
-//            throw new DataAccessException("Error retrieving user from database.");
-//        }
-//    }
-//    @Override
-//    public void updateUser(UserData user){
-//        String sql = "UPDATE users SET password = ?, email = ? WHERE username = ?";
-//
-//        try (Connection conn = Database.getConnection();
-//             PreparedStatement stmt = conn.prepareStatement(sql)) {
-//
-//            stmt.setString(1, user.password());
-//            stmt.setString(2, user.email());
-//            stmt.setString(3, user.username());
-//
-//            stmt.executeUpdate();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+package dataaccess;
+
+import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class SQLUserDAO implements UserDAO {
+
+    public SQLUserDAO() {
+        try {
+            Database.createDatabase();
+        } catch (DataAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        try (Connection conn = Database.getConnection()) {
+            String createUsersTableSQL = """
+                    CREATE TABLE IF NOT EXISTS users (
+                        username VARCHAR(255) NOT NULL,
+                        password VARCHAR(255) NOT NULL,
+                        email VARCHAR(255),
+                        PRIMARY KEY (username)
+                    )""";
+
+            try (PreparedStatement createTableStatement = conn.prepareStatement(createUsersTableSQL)) {
+                createTableStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+
+        try (Connection conn = Database.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, user.username());
+            stmt.setString(2, user.password());
+            stmt.setString(3, user.email());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error inserting user into database.");
+        }
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        String sql = "SELECT username, password, email FROM users WHERE username = ?";
+
+        try (Connection conn = Database.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, username);
+            var rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new UserData(
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("email")
+                );
+            } else {
+                throw new DataAccessException("User not found.");
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error retrieving user from database.");
+        }
+    }
+//x
 //    @Override
 //    public void deleteUser(String username){
 //        String sql = "DELETE FROM users WHERE username = ?";
@@ -101,17 +88,31 @@
 //            throw new RuntimeException(e);
 //        }
 //    }
+
+    @Override
+    public void clear() {
+        String sql = "DELETE FROM users";
+
+        try (Connection conn = Database.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    void storeUserPassword(String username, String clearTextPassword) {
+//        String hashedPassword = BCrypt.hashpw(clearTextPassword, BCrypt.gensalt());
 //
-//    @Override
-//    public void clear() {
-//        String sql = "DELETE FROM users";
-//
-//        try (Connection conn = Database.getConnection();
-//             PreparedStatement stmt = conn.prepareStatement(sql)) {
-//
-//            stmt.executeUpdate();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
+//        // write the hashed password in database along with the user's other information
+//        writeHashedPasswordToDatabase(username, hashedPassword);
 //    }
-//}
+//
+//    boolean verifyUser(String username, String providedClearTextPassword) {
+//        // read the previously hashed password from the database
+//        var hashedPassword = readHashedPasswordFromDatabase(username);
+//
+//        return BCrypt.checkpw(providedClearTextPassword, hashedPassword);
+//    }
+}
